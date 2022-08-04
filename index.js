6e48b2b24bc2079dd080f3201058cf498f688838
Franco Scuro <franquitoscro@gmail.com>
2022-08-04 20:52:56 -0300

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Express } from "express";
+
+const express = require('express')
+const app = express();
+
+app.get('/', (req, resp) => {
+    resp.send("Mi primer backend en heroku")
+});
+
+app.listen(process.env.PORT || 4002, () => {
+    console.log("Funcionando")
+})
