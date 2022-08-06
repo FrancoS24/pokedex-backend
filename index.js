2a93999e344a6edd3fd4ab922d2e56e77df47958
Franco Scuro <franquitoscro@gmail.com>
2022-08-05 21:05:10 -0300

    v1

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: fakse}))
+app.use(bodyParser.json());
+app.use(cors());
+
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+
+
+
 
 app.get('/', (req, resp) => {
     resp.send("Mi primer backend en heroku")
